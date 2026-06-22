522a78af88b189439f1d835d8d8da42a1881319c
aurmatos <aura.matos18@gmail.com>
2026-06-22 16:43:57 -0400

    Automatiza el camino positivo de la historia de Login

---
+++ tests/login.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://automationexercise.com/signup');
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
+  await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('aurmatos@dgii.gov.do');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Password' }).fill('T');
+  await page.getByRole('textbox', { name: 'Password' }).press('CapsLock');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Test123');
+  await page.getByRole('button', { name: 'Login' }).click();
+});
